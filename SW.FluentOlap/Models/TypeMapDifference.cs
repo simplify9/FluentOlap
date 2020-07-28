e4449986a77f19d466334fad43cfad28a173e289
Shaheen Sarafa <shaheen@simplify9.com>
2020-07-28 10:54:25 +0300

    Added model for differences

---
+++ SW.FluentOlap/Models/TypeMapDifference.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.FluentOlap.Models
+{
+    public enum DifferenceType
+    {
+        DataTypeChange,
+        AddedColumn,
+        RemovedColumn
+    }
+    public class TypeMapDifference
+    {
+        private string columnKey;
+        public string ColumnKey { get => columnKey.ToLower(); set => columnKey = value.ToLower(); }
+        public DifferenceType DifferenceType { get; set; }
+
+        public TypeMapDifference() {}
+        public TypeMapDifference(string columnKey, DifferenceType differenceType)
+        {
+            ColumnKey = columnKey;
+            DifferenceType = differenceType;
+        }
+
+    }
+}
