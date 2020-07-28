f4862dedb9d0e61d3715ec269cdc57d5dd00aa74
Shaheen Sarafa <shaheen@simplify9.com>
2020-07-28 14:11:25 +0300

    Added base64 encoding and decoding to typemap

--- SW.FluentOlap/Models/TypeMap.cs
+++ SW.FluentOlap/Models/TypeMap.cs
@@ -39,6 +39,19 @@ namespace SW.FluentOlap.Models
 
         public bool ContainsKey(string key) => _typeMap.ContainsKey(key.ToLower());
 
+        public string EncodeToBase64()
+        {
+            byte[] mapBytes = Encoding.ASCII.GetBytes(this.ToString());
+            return Convert.ToBase64String(mapBytes);
+        }
+
+        public static TypeMap DecodeFromBase64(string base64)
+        {
+            TypeMap map = new TypeMap();
+            byte[] mapBytes = Convert.FromBase64String(base64);
+            string typeMapString = Encoding.ASCII.GetString(mapBytes);
+            return TypeMap.FromString(typeMapString);
+        }
 
         public void CopyTo(KeyValuePair<string, NodeProperties>[] array, int arrayIndex)
         {
@@ -54,6 +67,29 @@ namespace SW.FluentOlap.Models
         public bool TryGetValue(string key, out NodeProperties value) => _typeMap.TryGetValue(key, out value);
 
         IEnumerator IEnumerable.GetEnumerator() => _typeMap.GetEnumerator();
+
+        public static TypeMap FromString(string s)
+        {
+            TypeMap map = new TypeMap();
+            foreach(string segment in s.Split(','))
+            {
+                if (segment == "") continue;
+                string[] segArr = segment.Split(':');
+                map.Add(segArr[0], NodeProperties.FromString(segArr[1]));
+            }
+            return map;
+        }
+        public override string ToString()
+        {
+            string typeMapsString = string.Empty;
+            foreach(KeyValuePair<string, NodeProperties> map in this)
+            {
+                string mapString = $"{map.Key}:{map.Value},";
+                typeMapsString += mapString;
+            }
+            return typeMapsString;
+
+        }
 
     }
 }
