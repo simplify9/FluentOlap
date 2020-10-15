daeb24e8c282db2346b6e9c1f120464c312250b5
shaheen-s <shaheensarafa17@gmail.com>
2020-10-15 11:23:10 +0300

    Service refactoring

--- SW.FluentOlap/Models/Service.cs
+++ SW.FluentOlap/Models/Service.cs
@@ -1,14 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace SW.FluentOlap.Models
 {
-    public class Service
+    public enum ServiceType
     {
-        public string BaseUrl { get; set; }
-        public string Endpoint { get; set; }
-
-        //public Func<object, object> GetFunc { get; set; }
+        HttpCall,
+        DatabaseCall
     }
+    public interface IService
+    {
+        
+        public ServiceType ServiceType { get; }
+
+    }
+
+    public class HttpService : IService
+    {
+        /// <summary>
+        /// All strings between double curlies {{}} will be treated as Json Paths
+        /// And filled in during the Data collection.
+        /// This Url will override all other Url related properties if provided.
+        /// Example: https://someUrl.com/{Id}/comments/{comment.Id}
+        /// </summary>
+        public string TemplatedUrl { get; set; }
+
+        public ServiceType ServiceType => ServiceType.HttpCall;
+    }
+
+    public class DatabaseService : IService
+    {
+        
+        /// <summary>
+        /// All strings between double curlies {{}} will be treated as Json Paths
+        /// And filled in during the Data collection.
+        /// This Url will override all other Url related properties if provided.
+        /// Example: https://someUrl.com/{Id}/comments/{comment.Id}
+        /// </summary>
+        public string TemplatedSelect { get; set; }
+        public ServiceType ServiceType => ServiceType.HttpCall;
+    }
+    
+    
 }
