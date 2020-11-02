c093508491a3c049b93db5f0469306625100191c
shaheen-s <shaheensarafa17@gmail.com>
2020-11-02 11:53:47 +0200

    Refactor

--- SW.FluentOlap/Models/AnalyticalObjectInitializationSettings.cs
+++ SW.FluentOlap/Models/AnalyticalObjectInitializationSettings.cs
@@ -4,8 +4,16 @@ namespace SW.FluentOlap.Models
 {
     public class AnalyticalObjectInitializationSettings<T>
     {
+        public AnalyticalObjectInitializationSettings()
+        {
+            _referenceLoopDepthLimit = 1;
+        }
         private const byte REFERENCELOOPSYSTEMLIMIT = 3;
         private byte _referenceLoopDepthLimit;
+        /// <summary>
+        /// How many times the same type can be referenced in a chain of properties.
+        /// Maximum of 3, default of 1. 0 would disable self referencing.
+        /// </summary>
         public byte ReferenceLoopDepthLimit
         {
             get => _referenceLoopDepthLimit;
